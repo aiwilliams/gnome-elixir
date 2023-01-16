d2b0e20995f00bcccd3a16f4f55105dbb2481b79
Adam Williams <adam.williams@jupiterone.com>
2023-01-16 17:00:38 -0500

    Test won and lost game

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -41,16 +41,41 @@ defmodule Hangman.Impl.Game do
 
   defp accept_guess(game, guess, _already_used) do
     %{game | used: MapSet.put(game.used, guess)}
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+
+  defp score_guess(game, _good_guess = true) do
+    new_state = guess_state(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{game | game_state: new_state}
+  end
+
+  defp score_guess(game = %{turns_left: 1}, _bad_guess) do
+    %{game | game_state: :lost, turns_left: 0}
+  end
+
+  defp score_guess(game, _bad_guess) do
+    %{game | game_state: :bad_guess, turns_left: game.turns_left - 1}
+  end
+
+  defp guess_state(true), do: :won
+  defp guess_state(_), do: :good_guess
 
   defp tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> letter_or_underscore(letter) end)
+  end
+
+  defp letter_or_underscore(true, letter), do: letter
+  defp letter_or_underscore(_, _), do: "_"
 
   defp return_with_tally(game) do
     {game, tally(game)}
